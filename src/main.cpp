1eb4412d7745d53cd5dc0d2fe687a10b6a6e43fe
Gregwar <g.passault@gmail.com>
2020-10-25 14:11:53 +0100

    Using rhoban shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include "mbed.h"
-#include "USBSerial.h"
+#include "shell.h"
 
 DigitalOut led(PC_13);
-USBSerial serial;
+
+SHELL_PARAMETER_FLOAT(a, "A", 0.)
+SHELL_PARAMETER_FLOAT(b, "B", 0.)
+
+SHELL_COMMAND(sum, "Sum")
+{
+    shell_print("Sum: ");
+    shell_print(a+b);
+    shell_println();
+}
 
 int main()
 {
+    shell_init_usb();
+
     while(1) {
-        serial.printf("Hello!\r\n");
         ThisThread::sleep_for(500);
         led = 0;
         ThisThread::sleep_for(500);
